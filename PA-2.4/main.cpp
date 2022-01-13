783b3042aa7096a13bf9ed7ade8a8d7e0a2f2258
powellcaleb <46825847+powellcaleb@users.noreply.github.com>
2022-01-12 19:18:03 -1000

    Initial Commit

---
+++ PA-2.4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PA-2.4
+//
+//  Created by Caleb Daniel Powell on 1/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
